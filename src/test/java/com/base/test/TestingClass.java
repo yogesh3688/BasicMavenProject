1b8fa0f395bd3f06d191284e2c8cb9ecf7f824c6
Yogesh <yogeshgnipate@yahoo.com>
2024-04-01 22:28:36 +0530

    dev

---
+++ src/test/java/com/base/test/TestingClass.java
@@ -0,0 +1,12 @@
+package com.base.test;
+
+import org.testng.annotations.Test;
+
+public class TestingClass {
+
+	@Test
+	public void testOne() {
+		System.out.println("This is test method");
+	}
+	
+}
